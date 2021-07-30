95e5e4cb856a1aa028454120ea548286391de831
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-07-29 23:35:19 -0500

    feat: agregar usuarios prueba

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -45,7 +45,7 @@ namespace API.Data
 
                 using var hmac = new HMACSHA512();
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("password"));
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("P@ssword"));
                 user.PasswordSalt = hmac.Key;
                 context.Users.Add(user);
 
